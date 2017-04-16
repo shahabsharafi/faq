9aa601253767cc4b52162a40d121c60c4d167077
shahabsharafi <shahab.sharafi@gmail.com>
2017-04-16 18:13:44 +0430

    change user model

--- server/features/user/user.js
+++ server/features/user/user.js
@@ -5,5 +5,34 @@ var Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
     username: String,
-    password: String
+    password: String,
+    person: {
+        firstName: String,
+        lastName: String,
+        fatherName: String,
+        birthDay: Date,
+        no: String,
+        placeOfIssues: String,
+        nationalCode: String,
+        birthPlace: String,
+        status: Number
+    },
+    contact: {
+        mobile: String,
+        house: String,
+        work: String,
+        email: String,
+        contry: String,
+        city: String,
+        address: String,
+        pcode: String
+    },
+    education: {
+        grade: String,
+        majors: String,
+        universities: String,
+        level: String
+    },
+    language: Number,
+    dialect: Number
 }));
