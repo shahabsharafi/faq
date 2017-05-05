5a27b0117bf580a4b440b220737fce5e3b8b694c
shahabsharafi <shahab.sharafi@gmail.com>
2017-05-05 08:58:33 +0430

    change folder

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from './login/index';
+import { LoginComponent } from './components/login/index';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import {AccountListComponent} from './components/account-list/account-list.component';
 import { AuthGuard } from './guards/index';
