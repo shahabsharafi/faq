c4044dbf7f0d6d988c5bffad40967a3e60c2b657
shahabsharafi <shahab.sharafi@gmail.com>
2017-06-11 11:40:18 +0430

    add discotion

---
+++ server/features/discussion/discussion.js
@@ -0,0 +1,38 @@
+// get an instance of mongoose and mongoose.Schema
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+var schema = new mongoose.Schema({
+    title: String,
+    from: {
+        type: String,
+        ref: 'Account'
+    },
+    to: {
+        type: String,
+        ref: 'Account'
+    },
+    state: {
+        type: String,
+        ref: 'Attribute'
+    },
+    department: {
+        type: String,
+        ref: 'Department'
+    },
+    createDate: Date,
+    expDate: Date,
+    content: [{
+        owner: {
+            type: String,
+            ref: 'Account'
+        },
+        createDate: Date,
+        text: String
+    }]
+});
+
+schema.plugin(mongoosePaginate);
+
+// set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('Discussion', schema);
