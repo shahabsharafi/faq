060e48f6045586fdeb111f6686183daae39a82c7
shahabsharafi <shahab.sharafi@gmail.com>
2017-07-18 10:25:26 +0430

    correct accounting error

--- server/features/accounting/index.js
+++ server/features/accounting/index.js
@@ -1,99 +1,99 @@
 var register = function (option) {
 
-        const Repository = require('../_infrastructure/repository');
-        const controller = require('../_infrastructure/controller');
-        const utility = require('../_infrastructure/utility');
-        const Accounting = require('./accounting');
-        const URL = require('url');
-        const parser = require("odata-parser");
+    const Repository = require('../_infrastructure/repository');
+    const controller = require('../_infrastructure/controller');
+    const utility = require('../_infrastructure/utility');
+    const Accounting = require('./accounting');
+    const URL = require('url');
+    const parser = require("odata-parser");
 
-        var router = option.express.Router();
-        var repository = new Repository(Accounting);
+    var router = option.express.Router();
+    var repository = new Repository(Accounting);
 
-        controller(router, Accounting, repository, null);
+    controller(router, Accounting, repository, null);
 
-        router.post('/transaction', function (req, res) {
-                var attrs = {};
+    router.post('/transaction', function (req, res) {
+        var attrs = {};
 
-                var _part1 = function (callback) {
-                    if (req.body.account) {
-                        req.body.account = req.body.account._id;
-                    } else {
-                        Account.findOne({
-                            username: req.decoded._doc.username
-                        }, function (err, obj) {
-                            if (err) {
-                                callback(err)
-                            } else {
-                                req.body.account = obj;
-                                callback()
-                            }
-                        });
-                    }
-                }
-
-                var _part2 = function (callback) {
-                    var obj = new Accounting(req.body)
-                    repository.Save(req.body, function (err) {
-                        if (callback) callback(err);
-                    });
-                }
-
-                utility.taskRunner([_part1, _part2], function (err) {
+        var _part1 = function (callback) {
+            if (req.body.account) {
+                req.body.account = req.body.account._id;
+            } else {
+                Account.findOne({
+                    username: req.decoded._doc.username
+                }, function (err, obj) {
                     if (err) {
-                        res.status(500).send(err);
+                        callback(err)
                     } else {
-                        res.json({
-                            success: true
-                        });
+                        req.body.account = obj;
+                        callback()
                     }
                 });
             }
-
-            router.get('/setup', function (req, res) {
-
-                var attrs = {};
-
-                var _part1 = function (callback) {
-                    Accounting.remove({}, callback);
-                }
-
-                var _part2 = function (callback) {
-                    Account.findOne({
-                        username: 'admin'
-                    }, function (err, obj) {
-                        if (err) {
-                            callback(err)
-                        } else {
-                            attrs.account = obj;
-                            callback()
-                        }
-                    });
-                }
-
-                var _part3 = function (callback) {
-                    var obj = new Accounting({
-                        createDate: new Date(),
-                        debit: 187366000,
-                        credit: 0,
-                        description: 'پرداخت پول',
-                        account: attrs.account
-                    });
-                    repository.Setup(obj, callback);
-                }
-
-                utility.taskRunner([_part1, _part2, _part3], function (err) {
-                    if (err) res.send(err);
-                    res.json({
-                        success: true
-                    });
-                });
-            });
-
-            option.app.use('/api/accountings', router);
         }
 
-        module.exports = {
-            name: 'accounting',
-            register: register
-        };
+        var _part2 = function (callback) {
+            var obj = new Accounting(req.body)
+            repository.Save(req.body, function (err) {
+                if (callback) callback(err);
+            });
+        }
+
+        utility.taskRunner([_part1, _part2], function (err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json({
+                    success: true
+                });
+            }
+        });
+    });
+
+    router.get('/setup', function (req, res) {
+
+        var attrs = {};
+
+        var _part1 = function (callback) {
+            Accounting.remove({}, callback);
+        }
+
+        var _part2 = function (callback) {
+            Account.findOne({
+                username: 'admin'
+            }, function (err, obj) {
+                if (err) {
+                    callback(err)
+                } else {
+                    attrs.account = obj;
+                    callback()
+                }
+            });
+        }
+
+        var _part3 = function (callback) {
+            var obj = new Accounting({
+                createDate: new Date(),
+                debit: 187366000,
+                credit: 0,
+                description: 'پرداخت پول',
+                account: attrs.account
+            });
+            repository.Setup(obj, callback);
+        }
+
+        utility.taskRunner([_part1, _part2, _part3], function (err) {
+            if (err) res.send(err);
+            res.json({
+                success: true
+            });
+        });
+    });
+
+    option.app.use('/api/accountings', router);
+}
+
+module.exports = {
+    name: 'accounting',
+    register: register
+};
