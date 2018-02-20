ce36444de0e77630e7ad359706efc4fe1c4219a5
shahabsharafi <shahab.sharafi@gmail.com>
2018-02-20 18:50:42 +0330

    peyment page style

---
+++ mobile/main.js
@@ -0,0 +1,24 @@
+var getParameterByName = function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var getUser = function () {
+    var key = getParameterByName('key');
+    var request = new XMLHttpRequest();
+    request.open("get", "/api/accounts/finduserbykey?key=" + key, false);
+    request.send();
+    var obj = JSON.parse(request.responseText);
+    return obj.message;
+}
+
+var go = function () {
+    var user = getUser();
+    var message = 'user: ' + user + ' chash: ' + document.getElementById('txt').value;
+    alert(message);
+}
